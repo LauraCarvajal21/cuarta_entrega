c73bf2c43cf536d3e3849bc0555afaf37f96ad32
LauraCarvajal21 <lvcarvajalv@correo.udistrital.edu.co>
2022-06-27 12:36:36 -0500

    Como no laps

--- screens/laps.py
+++ screens/laps.py
@@ -13,15 +13,28 @@ class Laps():
         self.BOTON_SUBIR = 14
         self.BOTON_BAJAR = 4
         self.BOTON_ENTER = 25
+
+        self.PinN1 = 23
+        self.PinN2 = 24
+        self.PWM = 18
+
         GPIO.setup(self.BOTON_SUBIR, GPIO.IN)
         GPIO.setup(self.BOTON_BAJAR, GPIO.IN)
         GPIO.setup(self.BOTON_ENTER, GPIO.IN)
+        GPIO.setup(self.PWM, GPIO.OUT)
+        GPIO.setup(self.PinN1, GPIO.OUT)
+        GPIO.setup(self.PinN2, GPIO.OUT)
+
+
+
 
     def __show(self):
         self.my_lcd.lcd_clear()
         self.my_lcd.lcd_display_string(f'Vueltas: {self.laps}', 1)
 
     def show_and_run(self):
+        pwm= GPIO.PWM(self.PWM,500)
+        pwm.start(0)
         self.__show()
         while self.is_running:
             if GPIO.input(self.BOTON_BAJAR) and self.laps != 0:
@@ -38,6 +51,17 @@ class Laps():
                 while GPIO.input(self.BOTON_ENTER):
                     pass
                 self.my_lcd.lcd_clear()
+
+                pwm.ChangeDutyCycle(self.speed)
+
+
+                if self.direction==0:
+                    GPIO.output(self.IN1,False)
+                    GPIO.output(self.IN2,True)
+                else:
+                    GPIO.output(self.IN1,True)
+                    GPIO.output(self.IN2,False)
+
                 while self.laps != 0:
                     self.my_lcd.lcd_display_string(f'Quedan: {self.laps}', 1)
                     time.sleep(1)
