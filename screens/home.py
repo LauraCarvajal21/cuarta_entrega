c2888eff132a95300ed6c1ad2d58ca35cf229fb4
LauraCarvajal21 <lvcarvajalv@correo.udistrital.edu.co>
2022-06-27 10:06:14 -0500

    home

--- screens/home.py
+++ screens/home.py
@@ -5,12 +5,12 @@ class Home():
     def __init__(self, lcd) -> None:
         self.is_running = True
         self.my_lcd = lcd
-        self.OPTIONS = [{'option': 1, 'text': 'PWM'},
-                        {'option': 2, 'text': 'DIRECCION'},
-                        {'option': 3, 'text': '# VUELTAS'}]
+        self.OPTIONS = ['PWM', 'DIRECCION', '# VUELTAS']
         self.BOTON_SUBIR = 14
         self.BOTON_BAJAR = 4
         self.BOTON_ENTER = 25
+
+        self.position = 0
 
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.BOTON_SUBIR, GPIO.IN)
@@ -18,10 +18,26 @@ class Home():
         GPIO.setup(self.BOTON_ENTER, GPIO.IN)
         GPIO.setwarnings(False)
 
-    def __show_menu(self):
+    def __show_menu(self, bajando):
         self.my_lcd.lcd_clear()
-        self.my_lcd.lcd_display_string(self.OPTIONS[0].get('text'),1,3)
-        self.my_lcd.lcd_display_string(self.OPTIONS[1].get('text'),2,3)
-        self.my_lcd.lcd_display_string('->',1)
+
+        new_option = self.OPTIONS[self.position -
+                                  bajando:self.position+2-bajando]
+
+        self.my_lcd.lcd_display_string(new_option[0], 1, 3)
+        self.my_lcd.lcd_display_string(new_option[2], 1, 3)
+
+        self.my_lcd.lcd_display_string('->', bajando+1)
+
     def show(self):
         self.__show_menu()
+        if GPIO.input(self.BOTON_BAJAR):
+            self.position += 1
+            while GPIO.input(self.BOTON_BAJAR):
+                pass
+            self.__show_menu(1)
+        if GPIO.input(self.BOTON_SUBIR):
+            self.position -= 1
+            while GPIO.input(self.BOTON_BAJAR):
+                pass
+            self.__show_menu(0)
